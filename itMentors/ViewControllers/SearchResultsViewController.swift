1cf22d7997fb470d993154f1bbd701d15b20662c
i_drozhzh <idrozhzh@gmail.com>
2022-08-01 16:17:31 +0300

    Setup the mentor image on search results screen

--- itMentors/ViewControllers/SearchResultsViewController.swift
+++ itMentors/ViewControllers/SearchResultsViewController.swift
@@ -16,27 +16,28 @@ class SearchResultsViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return mentorsList.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "mentorCell", for: indexPath)
 
         let mentor = mentorsList[indexPath.row]
         var content = cell.defaultContentConfiguration()
         
+        cell.frame.size.height = 100
+        
         content.image = UIImage(named: mentor.image)
+        content.imageProperties.cornerRadius = cell.frame.height / 2
         content.text = mentor.fullname
+
         
         cell.contentConfiguration = content
 
         return cell
     }
 
-    
     // MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let indexPath = tableView.indexPathForSelectedRow {
