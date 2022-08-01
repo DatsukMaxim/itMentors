49009b37ff7f5bc70b565b5a25cf9c8f9b6461fa
TheRenat <therenat@mail.ru>
2022-08-01 16:51:18 +0300

    add tabBarVC

---
+++ itMentors/ViewControllers/MentorDetailsTabBarControllerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MentorDetailsTabBarControllerViewController.swift
+//  itMentors
+//
+//  Created by Renat Ibragimov on 01.08.2022.
+//
+
+import UIKit
+
+class MentorDetailsTabBarControllerViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewControllers()
+    }
+    
+    private func setupViewControllers() {
+        //guard let mentorVC = viewControllers?.first as? MentorInfoViewController else { return }
+        //guard let reviewsVC = viewControllers?.last as? ReviewsTableViewController else { return }
+    }
+}
