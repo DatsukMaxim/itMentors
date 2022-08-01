7edec0ebe821825cf86c48674f74bbb4e27fe941
i_drozhzh <idrozhzh@gmail.com>
2022-08-01 22:02:59 +0300

    Resolve after feedback

--- itMentors/Sources/MentorDataStore.swift
+++ itMentors/Sources/MentorDataStore.swift
@@ -49,15 +49,12 @@ class MentorDataStore {
     "efimov", "gates", "ive",
     "musk"
     ]
-    
-    
     let scopes = [
         "Swift", "Objective-C", "Python",
         "JavaScript", "C#", "C++",
         "Kotlin", "PHP", "Delphi",
         "Golang", "Java"
     ]
-    
     let reviews = [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu",
