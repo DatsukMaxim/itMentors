0471fc7d3e614c423643c0513fb4a5f697c57d7a
Renat Ibragimov <107434090+TheRenat@users.noreply.github.com>
2022-08-02 14:14:01 +0300

    Update StudentsTabBarViewController.swift

--- itMentors/ViewControllers/StudentsTabBarViewController.swift
+++ itMentors/ViewControllers/StudentsTabBarViewController.swift
@@ -11,10 +11,6 @@ class StudentsTableViewController: UITableViewController {
 
     var mentor: Mentor!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return Int.random(in: 1...mentor.students.count)
